d41fc94b3d9922ea294c750a370f310d1b13a432
Conor Turner <conor_turner@hotmail.com>
2025-11-02 22:25:01 +0000

    Cashbuilder charts

--- analyse_data.py
+++ analyse_data.py
@@ -9,6 +9,60 @@ episodes = pd.read_csv('episodes.csv')
 # work out some supplementary info - aggregate data for episodes (let's not scrape it, we should have everything we need in here no?)
 players['won_hth'] = players['hth_winner'] == 'PLAYER'
 
+# question 0: What is the average cashbuilder score?
+cash_builder = players['cash_builder'].value_counts().sort_index()
+max_count = cash_builder.values.max()
+plt.figure()
+plt.title('Cash Builder Score Distribution')
+plt.xlabel('Cash Builder Score (£k)')
+plt.xticks(range(0, 15, 1))
+plt.ylabel('Number of Players')
+plt.yticks(range(0, max_count + 1, 100))
+
+bars = plt.bar(cash_builder.index / 1000, cash_builder.values, width=0.9)
+for bar in bars:
+    h = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, h + max_count * 0.01, f'{int(h):,}', ha='center', va='bottom', fontsize=8)
+
+plt.savefig('cashbuilder_distribution.png')
+
+
+# Question 0.5: how does cashbuilder score correlate with high / low offers?
+config = {
+    'lower_offer': {
+        'title': 'Lower Offer Value',
+        'ymin': -20,
+        'ymax': 11,
+        'ygap': 2,
+    },
+    # 'cash_builder': {
+    #     'title': 'Cash Builder Score',
+    #     'ymax': 130_000,
+    # },
+    'higher_offer': {
+        'title': 'Higher Offer Value',
+        'ymin': 0,
+        'ymax': 131,
+        'ygap': 10,
+    },
+}
+for column in config:
+    plt.figure()
+    for label, data in players.groupby('player_number'):
+        # stagger the x-values based on player number. 1 = -0.5, 4 = +0.5
+        x_offset = (label - 2.5) * 0.15
+        x = data['cash_builder'] / 1000 + x_offset
+        plt.scatter(x, data[column] / 1000, alpha=0.25, label=f'Player {label}', s=5)
+
+    plt.title(f'Cash Builder vs {config[column]["title"]}')
+    plt.xlabel('Cash Builder Score (£k)')
+    plt.xticks(range(15))
+    plt.ylabel(f'{config[column]["title"]} (£k)')
+    plt.yticks(range(config[column]['ymin'], config[column]['ymax'], config[column]['ygap']))
+    plt.grid(True, linestyle='--')
+    plt.legend()
+    plt.savefig(f'cashbuilder_vs_{column}.png')
+
 # Question 1: what are the get-home percentages for the lower / higher offers?
 higher = players.loc[players['chosen_offer'] == 1]
 higher_win_pc = higher['won_hth'].mean() * 100
